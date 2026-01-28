6aefc93ef2f8433adabc776128348bdeb8e1f1ea
AndreGabrielle <135079990+AndreGabrielle@users.noreply.github.com>
2026-01-28 14:44:10 +0800

    Add files via upload

---
+++ Attendance_Sysytem.py
@@ -0,0 +1,11 @@
+import qrcode
+import cv2
+import pandas as pd
+import os
+import json
+from datetime import datetime
+import tkinter as tk
+from tkinter import ttk, messagebox, filedialog
+from PIL import Image, ImageTk
+import threading
+import time
